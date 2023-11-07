79888dc62cb86ecedaa597069c556a0cd5b901d6
Piotr Instunajd <p.instunajd@wp.pl>
2023-11-07 12:15:51 +0100

    Second

---
+++ Entities/Hotele.cs
@@ -0,0 +1,15 @@
+﻿namespace Hotelum.Entities
+{
+    public class Hotele
+    {
+        public int Id { get; set; }
+        public string Name {  get; set; }
+        public string Description { get; set; }
+        public string Category {  get; set; }
+        public int NumberOfRooms {  get; set; }
+        public bool Open { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactNumber { get; set; }
+        public int 
+    }
+}
